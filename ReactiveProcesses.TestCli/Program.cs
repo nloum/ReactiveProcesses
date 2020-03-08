c48d08cf9721a45fc6e6add6a2640910494ab44b
nloum <39776302+nloum@users.noreply.github.com>
2020-03-08 14:43:51 -0500

    Flesh out a basic stderr stdout API and prepare for release

--- ReactiveProcesses.TestCli/Program.cs
+++ ReactiveProcesses.TestCli/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
 
 namespace ReactiveProcesses.TestCli
 {
@@ -10,7 +14,19 @@ namespace ReactiveProcesses.TestCli
             
             var factory = new ReactiveProcessFactory();
             var proc = factory.Start("fswatch", $"-0 \"{Environment.CurrentDirectory}\"");
-            proc.StandardOutput.Subscribe(x =>
+            proc.StandardOutput
+                .Scan(new { StringBuilder = new StringBuilder(), BuiltString = (string)null },
+                    (state, ch) =>
+                    {
+                        if (ch == 0)
+                        {
+                            return new { StringBuilder = new StringBuilder(), BuiltString = state.StringBuilder.ToString() };
+                        }
+
+                        state.StringBuilder.Append(ch);
+                        return new { state.StringBuilder, BuiltString = (string)null };
+                    }).Where(state => state.BuiltString != null).Select(state => state.BuiltString)
+                .Subscribe(x =>
             {
                 Console.WriteLine(x);
             });
--- ReactiveProcesses/ReactiveProcess.cs
+++ ReactiveProcesses/ReactiveProcess.cs
@@ -28,36 +28,37 @@ namespace ReactiveProcesses
 
             _process.EnableRaisingEvents = true;
 
-            // TODO - come up with a better way of providing standard error and standard output that doesn't always assume
-            // everything will be newline-delimited. E.g., fswatch uses NULL delimiters.
+            // TODO - validate performance on sending each character from StandardOutput and StandardError as separate events.
+            // The other choice is to use ReadLine or the events, both of which assume valid string data.
+
+            // StandardOutput = Observable.FromEventPattern<DataReceivedEventHandler, DataReceivedEventArgs>(
+            //         handler => _process.OutputDataReceived += handler,
+            //         handler => _process.OutputDataReceived -= handler)
+            //     .Select(x => x.EventArgs.Data);
+            //
+            // StandardError = Observable.FromEventPattern<DataReceivedEventHandler, DataReceivedEventArgs>(
+            //         handler => _process.ErrorDataReceived += handler,
+            //         handler => _process.ErrorDataReceived -= handler)
+            //     .Select(x => x.EventArgs.Data);
+
+            StandardOutput = ReadLines(_process.StandardOutput).ToObservable(scheduler);
+            StandardError = ReadLines(_process.StandardError).ToObservable(scheduler);
             
-            StandardOutput = Observable.FromEventPattern<DataReceivedEventHandler, DataReceivedEventArgs>(
-                    handler => _process.OutputDataReceived += handler,
-                    handler => _process.OutputDataReceived -= handler)
-                .Select(x => x.EventArgs.Data);
-
-            StandardError = Observable.FromEventPattern<DataReceivedEventHandler, DataReceivedEventArgs>(
-                    handler => _process.ErrorDataReceived += handler,
-                    handler => _process.ErrorDataReceived -= handler)
-                .Select(x => x.EventArgs.Data);
-
-            // StandardOutput = ReadLines(_process.StandardOutput).ToObservable(scheduler);
-            // StandardError = ReadLines(_process.StandardError).ToObservable(scheduler);
             ExitCode = Observable.FromEventPattern<EventHandler, EventArgs>(handler => _process.Exited += handler,
                     handler => _process.Exited -= handler, scheduler).Select(ep => _process.ExitCode)
                 .Take(1);
         }
 
-        private IEnumerable<string> ReadLines(StreamReader reader)
+        private IEnumerable<char> ReadLines(StreamReader reader)
         {
             while (!reader.EndOfStream)
             {
-                yield return reader.ReadLine();
+                yield return (char) reader.Read();
             }
         }
         
-        public IObservable<string> StandardOutput { get; }
-        public IObservable<string> StandardError { get; }
+        public IObservable<char> StandardOutput { get; }
+        public IObservable<char> StandardError { get; }
         public IObservable<int> ExitCode { get; }
 
         public void OnCompleted()
